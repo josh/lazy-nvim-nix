ae2f3ff3416bb0de12ce1dc19c9aa25020839fec
Joshua Peek <josh@users.noreply.github.com>
2024-07-31 17:38:38 +0000

    Add treefmt

--- flake.nix
+++ flake.nix
@@ -3,13 +3,37 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+    }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      eachSystem = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+    in
+    {
+      packages = eachSystem (pkgs: {
+        inherit (nixpkgs.legacyPackages.${pkgs.system}) hello;
+        default = self.packages.${pkgs.system}.hello;
+      });
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+      checks = eachSystem (pkgs: {
+        formatting = treefmtEval.${pkgs.system}.config.build.check self;
+      });
+    };
 }
---
+++ treefmt.nix
@@ -0,0 +1,15 @@
+{
+
+  projectRootFile = "flake.nix";
+  programs = {
+    actionlint.enable = true;
+    deadnix.enable = true;
+    nixfmt.enable = true;
+    prettier.enable = true;
+    shellcheck.enable = true;
+    shfmt.enable = true;
+    statix.enable = true;
+    stylua.enable = true;
+    taplo.enable = true;
+  };
+}
