6f993991701111f8dfba7ac1a548a0668f718cdf
Joshua Peek <josh@users.noreply.github.com>
2024-08-02 20:57:39 +0000

    Revert "Add bad import to test allow-import-from-derivation"
    This reverts commit 643c44b1fb119ff5672a4bc949ee4b6cbb587484.

--- flake.nix
+++ flake.nix
@@ -29,20 +29,6 @@
     {
       inherit lib;
 
-      bad =
-        let
-          drv = derivation {
-            name = "hello";
-            builder = "/bin/sh";
-            args = [
-              "-c"
-              "echo -n hello >$out"
-            ];
-            system = "x86_64-linux";
-          };
-        in
-        "${builtins.readFile drv} world";
-
       packages = eachSystem (pkgs: {
         default = self.packages.${pkgs.system}.nvim;
 
