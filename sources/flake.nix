64c1448db078d78eb1037f810e515ef35a1442b9
Joshua Peek <josh@users.noreply.github.com>
2024-09-18 03:44:48 +0000

    Add explicit branches

--- sources/flake.nix
+++ sources/flake.nix
@@ -4,15 +4,15 @@
 {
   inputs = {
     "bufferline.nvim".flake = false;
-    "bufferline.nvim".url = "github:akinsho/bufferline.nvim";
+    "bufferline.nvim".url = "github:akinsho/bufferline.nvim/main";
     "catppuccin".flake = false;
-    "catppuccin".url = "github:catppuccin/nvim";
+    "catppuccin".url = "github:catppuccin/nvim/main";
     "lazy.nvim".flake = false;
-    "lazy.nvim".url = "github:folke/lazy.nvim";
+    "lazy.nvim".url = "github:folke/lazy.nvim/main";
     "LazyVim".flake = false;
-    "LazyVim".url = "github:LazyVim/LazyVim";
+    "LazyVim".url = "github:LazyVim/LazyVim/main";
     "lualine.nvim".flake = false;
-    "lualine.nvim".url = "github:nvim-lualine/lualine.nvim";
+    "lualine.nvim".url = "github:nvim-lualine/lualine.nvim/master";
   };
 
   outputs = _inputs: { };
