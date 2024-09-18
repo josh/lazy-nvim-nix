a04731ff8ce329d0f0cf02214e32865eb1eb96aa
Joshua Peek <josh@users.noreply.github.com>
2024-09-18 03:40:59 +0000

    Easier to sort lines like this

--- sources/flake.nix
+++ sources/flake.nix
@@ -3,26 +3,16 @@
 # "github:NixOS/nixpkgs/nixpkgs-unstable"
 {
   inputs = {
-    "bufferline.nvim" = {
-      url = "github:akinsho/bufferline.nvim";
-      flake = false;
-    };
-    "catppuccin" = {
-      url = "github:catppuccin/nvim";
-      flake = false;
-    };
-    "lazy.nvim" = {
-      url = "github:folke/lazy.nvim";
-      flake = false;
-    };
-    "LazyVim" = {
-      url = "github:LazyVim/LazyVim";
-      flake = false;
-    };
-    "lualine.nvim" = {
-      url = "github:nvim-lualine/lualine.nvim";
-      flake = false;
-    };
+    "bufferline.nvim".flake = false;
+    "bufferline.nvim".url = "github:akinsho/bufferline.nvim";
+    "catppuccin".flake = false;
+    "catppuccin".url = "github:catppuccin/nvim";
+    "lazy.nvim".flake = false;
+    "lazy.nvim".url = "github:folke/lazy.nvim";
+    "LazyVim".flake = false;
+    "LazyVim".url = "github:LazyVim/LazyVim";
+    "lualine.nvim".flake = false;
+    "lualine.nvim".url = "github:nvim-lualine/lualine.nvim";
   };
 
   outputs = _inputs: { };
