eac9c194e5919469eb3955c47c4b6b64e51d8588
Joshua Peek <josh@users.noreply.github.com>
2024-11-24 01:30:19 -0500

    Add updateScript

--- pkgs/lazyvim-plugins.nix
+++ pkgs/lazyvim-plugins.nix
@@ -1,18 +1,31 @@
 {
   lib,
+  stdenv,
   runCommand,
+  writeScript,
   neovim,
   jq,
   lazynvimPlugins,
   lazy-nvim ? lazynvimPlugins."lazy.nvim",
   LazyVim ? lazynvimPlugins."LazyVim",
 }:
-runCommand "lazyvim-plugins.json"
-  {
-    LAZY_PATH = lazy-nvim;
-    LAZYVIM_PATH = LazyVim;
-  }
-  ''
-    out=out.json ${lib.getExe neovim} -l ${./lazyvim-plugins.lua}
-    ${lib.getExe jq} --sort-keys <out.json >$out
-  ''
+let
+  updateScript = writeScript "update-LazyVim-json.sh" ''
+    #!${stdenv.shell}
+    set -o xtrace
+    install -m 644 ${pkg} plugins/LazyVim.json
+  '';
+  pkg =
+    runCommand "lazyvim-plugins.json"
+      {
+        LAZY_PATH = lazy-nvim;
+        LAZYVIM_PATH = LazyVim;
+
+        passthru.updateScript = updateScript;
+      }
+      ''
+        out=out.json ${lib.getExe neovim} -l ${./lazyvim-plugins.lua}
+        ${lib.getExe jq} --sort-keys <out.json >$out
+      '';
+in
+pkg
