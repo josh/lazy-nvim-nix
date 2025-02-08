89217c972a1a804dd0f05bfc9bc2ae93cd7712ac
Joshua Peek <josh@users.noreply.github.com>
2025-02-08 14:28:06 -0800

    ah, structuredAttrs doesn't export env by default

--- pkgs/neovim-checkhealth.nix
+++ pkgs/neovim-checkhealth.nix
@@ -12,48 +12,48 @@
   checkWarning ? false,
 }:
 let
-  lazyLoadCmd = if loadLazyPluginName != null then "+Lazy! load ${loadLazyPluginName}" else null;
+  lazyLoadCmd = if loadLazyPluginName != null then [ "+Lazy! load ${loadLazyPluginName}" ] else [ ];
   checkCmd =
-    if pluginName == null || pluginName == "all" then "+checkhealth" else "+checkhealth ${pluginName}";
+    if pluginName == null || pluginName == "all" then
+      [ "+checkhealth" ]
+    else
+      [ "+checkhealth ${pluginName}" ];
 in
 runCommand "checkhealth-${pluginName}"
   {
-    # __structuredAttrs = true;
+    __structuredAttrs = true;
 
-    NEOVIM_BIN = lib.getExe neovim;
-    CHECK_OK = if checkOk then "1" else "";
-    CHECK_ERROR = if checkError then "1" else "";
-    CHECK_WARNING = if checkWarning then "1" else "";
+    neovimBin = lib.getExe neovim;
+    nvimArgs =
+      [
+        "--headless"
+      ]
+      ++ lazyLoadCmd
+      ++ checkCmd
+      ++ [
+        "+w!out.txt"
+        "+q"
+      ];
 
-    LAZY_LOAD_CMD = lazyLoadCmd;
-    CHECK_CMD = checkCmd;
-
-    # FIXME: __structuredAttrs not working on Linux
-    # nvimArgs =
-    #   [
-    #     "--headless"
-    #   ]
-    #   ++ (lib.lists.optional (lazyLoadCmd != null) lazyLoadCmd)
-    #   ++ (lib.lists.optional (checkCmd != null) checkCmd)
-    #   ++ [
-    #     "+w!out.txt"
-    #     "+q"
-    #   ];
+    check = {
+      ok = checkOk;
+      error = checkError;
+      warning = checkWarning;
+    };
 
     nativeBuildInputs = lib.lists.optionals stdenv.isLinux [ xclip ];
-    DISPLAY = lib.optionalString stdenv.isLinux ":0";
-    LOCALE_ARCHIVE = lib.optionalString stdenv.isLinux "${glibcLocales}/lib/locale/locale-archive";
-    LANG = "en_US.UTF-8";
+
+    env = {
+      DISPLAY = lib.optionalString stdenv.isLinux ":0";
+      LOCALE_ARCHIVE = lib.optionalString stdenv.isLinux "${glibcLocales}/lib/locale/locale-archive";
+      LANG = "en_US.UTF-8";
+    };
   }
   ''
     mkdir -p .config/nvim
     touch .config/nvim/init.lua
 
-    if [ -n "$LAZY_LOAD_CMD" ]; then
-      HOME="$PWD" timeout 30s "$NEOVIM_BIN" --headless "$LAZY_LOAD_CMD" "$CHECK_CMD" '+w!out.txt' +q
-    else
-      HOME="$PWD" timeout 30s "$NEOVIM_BIN" --headless "$CHECK_CMD" '+w!out.txt' +q
-    fi
+    HOME="$PWD" timeout 30s "$neovimBin" "''${nvimArgs[@]}"
     cat out.txt
 
     ok_count=$(grep --count " OK " <out.txt || true)
@@ -61,13 +61,13 @@ runCommand "checkhealth-${pluginName}"
     warning_count=$(grep --count " WARNING " <out.txt || true)
     echo "$ok_count ok, $error_count errors, $warning_count warnings"
 
-    if [[ -n "$CHECK_ERROR" && "$error_count" -gt 0 ]]; then
+    if [[ -n "''${check[error]}" && "$error_count" -gt 0 ]]; then
       echo "Expected no errors, but were $error_count" >&2
       return 1
-    elif [[ -n "$CHECK_WARNING" && "$warning_count" -gt 0 ]]; then
+    elif [[ -n "''${check[warning]}" && "$warning_count" -gt 0 ]]; then
       echo "Expected no warnings, but were $warning_count" >&2
       return 1
-    elif [[ -n "$CHECK_OK" && "$ok_count" -eq 0 ]]; then
+    elif [[ -n "''${check[ok]}" && "$ok_count" -eq 0 ]]; then
       echo "Expected at least one OK" >&2
       return 1
     else
